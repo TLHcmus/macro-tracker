93a95c654142d4c7893b8e6ab9b8f8af28f203dc
TuLeWC <cubeaholic03@gmail.com>
2024-11-02 03:33:28 +0700

    Edit Food class

--- Model/Food.cs
+++ Model/Food.cs
@@ -10,11 +10,16 @@ namespace MacroTracker.Model;
 public class Food : INotifyPropertyChanged
 {
     public string Name { get; set; }
-    public int ServingSize { get; set; } // Vi du: 100g
-    public int Calories { get; set; }
-    public int Protein { get; set; }
-    public int Carbs { get; set; }
-    public int Fat { get; set; }
+    public int CaloriesPer100g { get; set; } 
+    public int ProteinPer100g { get; set; }
+    public int CarbsPer100g { get; set; }
+    public int FatPer100g { get; set; }
+
+    public (double Calories, double Protein, double Carbs, double Fat) GetNutrition(double grams)
+    {
+        double factor = grams / 100;
+        return (CaloriesPer100g * grams, ProteinPer100g * grams, CarbsPer100g * grams, FatPer100g * grams);
+    }
 
     public event PropertyChangedEventHandler PropertyChanged;  
 }
